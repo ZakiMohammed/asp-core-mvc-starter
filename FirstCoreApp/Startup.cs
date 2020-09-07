8d8d313d963ef086d02d1783f81a761f23e72264
zaki.shaikh <zaki.shaikh@vergehealth.com>
2020-09-07 19:54:17 +0530

    Comment updated

--- FirstCoreApp/Startup.cs
+++ FirstCoreApp/Startup.cs
@@ -24,7 +24,7 @@ namespace FirstCoreApp
             this.config = config;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method get called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
